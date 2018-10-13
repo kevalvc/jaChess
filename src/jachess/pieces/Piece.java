fd367a55ea0b2ee1577df412bc40199cf6a4a760
chiranjev <koulsahil04321@gmail.com>
2018-10-13 21:42:20 +0530

    stub for king,knight and pawn

--- src/jachess/pieces/Piece.java
+++ src/jachess/pieces/Piece.java
@@ -6,7 +6,46 @@ import java.util.ArrayList;
 
 public abstract class Piece implements Cloneable {
 
-    protected ArrayList<Cell> possiblemoves = new ArrayList<Cell>();
+    //Member Variables
+    private int color;
+    private String id = null;
+    private String path;
+    protected ArrayList<Cell> possiblemoves = new ArrayList<Cell>();  //Protected (access from child classes)
 
+    public abstract ArrayList<Cell> move(Cell pos[][], int x, int y);  //Abstract Function. Must be overridden
 
+    //Id Setter
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    //Path Setter
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    //Color Setter
+    public void setColor(int c) {
+        this.color = c;
+    }
+
+    //Path getter
+    public String getPath() {
+        return path;
+    }
+
+    //Id getter
+    public String getId() {
+        return id;
+    }
+
+    //Color Getter
+    public int getcolor() {
+        return this.color;
+    }
+
+    //Function to return the a "shallow" copy of the object. The copy has exact same variable value but different reference
+    public Piece getcopy() throws CloneNotSupportedException {
+        return (Piece) this.clone();
+    }
 }
